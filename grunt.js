2eb236999f1a8a94433f75f3ac123437e2308e23
VoQn <VoQn.tyrantist@gmail.com>
2012-09-02 21:16:32 +0900

    build script by grunt.js

---
+++ grunt.js
@@ -0,0 +1,62 @@
+/**
+ * @param {Object} grunt .
+ */
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    lint: {
+      files: [
+        'grunt.js',
+        'src/*.js',
+        'test/*.js'
+      ]
+    },
+    concat: {
+      dist: {
+        src: [
+          'src/init.coffee',
+          'src/util.coffee',
+          'src/trampoline.coffee',
+          'src/promise.coffee',
+          'src/mixins.coffee',
+          'src/each.coffee',
+          'src/map.coffee',
+          'src/fold.coffee',
+          'src/filter.coffee',
+          'src/any.coffee',
+          'src/whilist.coffee',
+          'src/task.coffee'
+        ],
+        dest: '_temp/pp.coffee'
+      }
+    },
+    coffee: {
+      'pp': {
+        dest: 'lib',
+        src: ['_temp/pp.coffee'],
+        options: {
+          bare: false
+        }
+      }
+    },
+    buster: {
+      test: {
+        config: './test/buster.js'
+      }
+    },
+    watch: {
+      files: [
+        'grunt.js',
+        'test/*.js',
+        'test/*.coffee',
+        'src/*.coffee'
+      ],
+      tasks: 'concat coffee lint buster'
+    }
+  });
+
+  grunt.registerTask('default', 'concat coffee lint buster');
+  grunt.loadNpmTasks('grunt-coffee');
+  grunt.loadNpmTasks('grunt-buster');
+};
+
